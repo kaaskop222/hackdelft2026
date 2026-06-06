182bf68be3ecd420c5c3dcb6ffb232abd5abfb2c
Jacek Kulik <macienias@gmail.com>
2026-06-06 16:35:45 +0200

    Add timre

--- src/timer.ts
+++ src/timer.ts
@@ -1,5 +1,17 @@
-class Timer {
-    time_remaining: Date
+export class Timer {
+    time_remaining_ms: number
+    last_timestamp: number
 
-    constructor(time_remaining)
+    constructor(total_time = 1000){
+        this.time_remaining_ms = total_time
+        this.last_timestamp = Date.now()
+        setInterval(() => this.natural_decay(), 10)
+    }
+
+    natural_decay() {
+        let current_timestamp = Date.now()
+        let time_passed = current_timestamp - this.last_timestamp
+        this.time_remaining_ms -= time_passed
+        this.last_timestamp = current_timestamp
+    }
 }
