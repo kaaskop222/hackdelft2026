d90516429b9b09211a798e64d1732f517fb4d6b0
Kvanoudenhoven <Keesvanoudenhoven@gmail.com>
2026-06-06 20:06:06 +0200

    stock update

--- public/ts/stock.ts
+++ public/ts/stock.ts
@@ -1,3 +1,10 @@
-class stock{
-    
-}
+import { log } from "node:console";
+import YahooFinance from "yahoo-finance2";
+
+const yahooFinance = new YahooFinance();
+
+const results = await yahooFinance.search("Apple");
+
+const quote = await yahooFinance.quote('AAPL');
+const { regularMarketPrice, currency } = quote;
+console.log(quote);
