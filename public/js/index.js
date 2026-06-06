c59cd000398a3dd087dd7f35a1724d4b0b57d35a
Jacek Kulik <macienias@gmail.com>
2026-06-06 18:42:19 +0200

    More timer functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,40 @@
 console.log("Hi!");
+let timer_text = document.querySelector("#timer");
+let subtract_input = document.querySelector("#subtract-input");
+let subtract_button = document.querySelector("#subtract-button");
+console.log(subtract_button);
+get_timer();
+let interval = setInterval(() => get_timer(), 20);
+subtract_button.addEventListener("click", () => subtract());
+function msToTime(duration) {
+    let multiplier = 1;
+    if (duration < 0)
+        multiplier = -1;
+    duration *= multiplier;
+    var milliseconds = Math.floor((duration % 1000) / 100), seconds = Math.floor((duration / 1000) % 60), minutes = Math.floor((duration / (1000 * 60)) % 60), hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    let hours_text = (hours < 10) ? "0" + hours : hours;
+    let minutes_text = (minutes < 10) ? "0" + minutes : minutes;
+    let seconds_text = (seconds < 10) ? "0" + seconds : seconds;
+    let multiplier_string = "";
+    if (multiplier < 0)
+        multiplier_string = "-";
+    return multiplier_string + hours_text + ":" + minutes_text + ":" + seconds_text + "." + milliseconds;
+}
+async function get_timer() {
+    let response = await fetch("/timer");
+    if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+    }
+    let result = await response.json();
+    timer_text.textContent = msToTime(result);
+    if (result == 0) {
+        timer_text.textContent = "Hello, World!";
+        clearInterval(interval);
+    }
+}
+async function subtract() {
+    let num_subtract = Number(subtract_input.innerHTML);
+    console.log(num_subtract);
+}
 export {};
 //# sourceMappingURL=index.js.map
--- src/timer.ts
+++ src/timer.ts
@@ -3,12 +3,14 @@ export class Timer {
     last_timestamp: number
     events: [string, number][]
     interval: NodeJS.Timeout
+    is_finished: boolean
 
-    constructor(total_time = 1000){
+    constructor(total_time = 10000){
         this.time_remaining_ms = total_time
         this.last_timestamp = Date.now()
         this.events = []
         this.interval = setInterval(() => this.natural_decay(), 10)
+        this.is_finished = false
     }
 
     natural_decay() {
@@ -16,12 +18,21 @@ export class Timer {
         let time_passed = current_timestamp - this.last_timestamp
         this.time_remaining_ms -= time_passed
         this.last_timestamp = current_timestamp
+        this.check_finished()
     }
 
     subtract(number_milliseconds: number, short_description: string) {
+        if (this.is_finished) return
         this.time_remaining_ms -= number_milliseconds
         this.events.push([short_description, number_milliseconds])
         setTimeout(() => this.events.shift(), 5000)
+        this.check_finished()
+    }
+
+    check_finished(){
+        if (this.time_remaining_ms < 0){
+            this.finished()
+        }
     }
 
     finished() {
