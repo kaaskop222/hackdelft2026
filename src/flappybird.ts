7bf3492ccc3bc49d0f87f64de40594f1fc7486ea
Kvanoudenhoven <Keesvanoudenhoven@gmail.com>
2026-06-06 17:10:34 +0200

    flappy.html

---
+++ src/flappybird.ts
@@ -0,0 +1,14 @@
+
+export class flappybird{
+    
+    private canvas: HTMLCanvasElement;
+    
+
+    constructor(){
+         let canvas = document.getElementById('canvas') as HTMLCanvasElement;
+
+        this.canvas = canvas;        
+    }
+
+    
+}
